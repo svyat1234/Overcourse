705bb96b2af8587f007c0ee6ab9e717578994618
unknown <svyat.teterin@gmail.com>
2025-04-30 20:07:59 +0300

    вёрстка главной

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
-const swiper = new Swiper('.swiper', {
+const impressionsSwiper = new Swiper('.impressions__swiper', {
     // Optional parameters
     direction: 'horizontal',
-    loop: false, // <--- Важно!
+    loop: true,
     slidesPerView: 'auto',
+    centeredSlides: true,
     spaceBetween: 30,
-    freeMode: true,
+    initialSlide: 3,
+    watchSlidesProgress: true,
     navigation: {
-        nextEl: '.swiper-button-next',
+        nextEl: '.impressions .swiper-button-next',
     },
-
 });
-  
-  
+
+const selectionSwiper = new Swiper('.selection__swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    watchSlidesProgress: true,
+    navigation: {
+        nextEl: '.selection .swiper-button-next',
+        prevEl: '.selection .swiper-button-prev',
+    },
+});
